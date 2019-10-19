49aa31bacc3f1586bd936149b51050fd0fda50c9
Italo Tavares <italotlima@hotmail.com>
2019-10-19 13:45:53 -0300

    Fix bug commit 38f104

--- system/Core/MainClassRequisiaco.js
+++ system/Core/MainClassRequisiaco.js
@@ -44,7 +44,7 @@ class Requisicao {
 
     processarErro(e) {
         `Não foi possível executar a controller. [${e.toString()}]`.GravarLog("error");
-        this.finalizaRequisicao({ok: false}, this.objetoController.headerResposta);
+        this.finalizaRequisicao({ok: false}, this.headerResposta);
     }
 
     async processarRequisicao() {
